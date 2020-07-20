2ab516d35233283e1055c1a3e8f19fbbbd70978a
Tristan Baumbusch <tristan.baumbusch@essquare.de>
2020-07-20 17:05:53 +0200

    feat: new setup with expo example

--- components/Decks.js
+++ components/Decks.js
@@ -6,7 +6,7 @@ export default class Decks extends React.Component {
   render() {
     return (
             <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-              <Text>Decks component</Text>
+              <Text>Decks component!!!</Text>
             </View>
     )
   }
